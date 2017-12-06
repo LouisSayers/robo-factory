94db4dd89308490ff25beec51a3f10aac1614e15
Louis Sayers <lssayers@gmail.com>
2017-12-06 15:39:46 +1100

    Set up React Router, with page headings

--- app/javascript/packs/robo_factory.tsx
+++ app/javascript/packs/robo_factory.tsx
@@ -1,15 +1,47 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import * as ReactRouterDOM from 'react-router-dom'
 
-interface HelloProps { name: string; }
+const { BrowserRouter, Redirect, Route, NavLink } = ReactRouterDOM
 
-const Hello = (props: HelloProps) => (
-  <div>Hello {props.name}!</div>
+const Sidebar = () => (
+  <nav className="col-sm-3 col-md-2 d-none d-sm-block sidebar">
+    <ul className="nav nav-pills flex-column">
+      <li className="subtitle">Dashboards</li>
+      <li className="nav-item">
+        <NavLink to="/batch/qa" className="nav-link">Quality Assurance</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/batch/shipping" className="nav-link">Shipping</NavLink>
+      </li>
+    </ul>
+  </nav>
 )
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="Robo Factory" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+const QualityAssurance = () => (
+  <main className="col-sm-9 ml-sm-auto col-md-10 pt-3">
+    <h1>Quality Assurance</h1>
+  </main>
+)
+
+const RobotShipping = () => (
+  <main className="col-sm-9 ml-sm-auto col-md-10 pt-3">
+    <h1>Shipping</h1>
+  </main>
+)
+
+const App = () => (
+  <BrowserRouter>
+    <div className="row">
+      <Sidebar />
+      <Route path='/batch/qa' component={QualityAssurance} />
+      <Route path='/batch/shipping' component={RobotShipping} />
+      <Route exact path="/" render={() => <Redirect to="/batch/qa" />} />
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('robo_factory')
+)
